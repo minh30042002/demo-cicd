6caac10d660e612063c08a77dfec8aee5cbb2e27
minh2103 <mnguyenhoang000@gmail.com>
2024-11-18 00:16:15 +0700

    update

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ $message = greetUser($name);
     <title>Simple PHP Page</title>
 </head>
 <body>
-    <h1>Welcome! T1 fighting</h1>
+    <h1>Welcome! Day la demo cicd cua nhom</h1>
     <p><?php echo $message; ?></p>
 </body>
 </html>
