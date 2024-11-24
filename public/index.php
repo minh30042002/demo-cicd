996f0cc1942e7103400738a653a692ec6c395dbc
minh2103 <mnguyenhoang000@gmail.com>
2024-11-25 05:33:26 +0700

    update

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ $message = greetUser($name);
     <title>Simple PHP Page</title>
 </head>
 <body>
-    <h1>Xin chao , Thay Vu dep trai vodich</h1>
+    <h1>Xin chao , T1 chien thang</h1>
     <p><?php echo $message; ?></p>
 </body>
 </html>
